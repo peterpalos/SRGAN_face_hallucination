01c59bec6ed1dea5b46d5d2b952224d0102e66a1
peterpalos <peterpalos333@gmail.com>
2019-11-19 16:39:20 +0100

    stage 2

--- image_processor.py
+++ image_processor.py
@@ -5,12 +5,12 @@ from PIL import Image
 image_name_list = listdir('original')
 
 target_size = (256, 256)
-source_size = (64, 64)
+input_size = (64, 64)
 for ind, im in enumerate(image_name_list):
     with Image.open('original/' + im) as image:
         image = ut.convert_image_mode_if_not_rgb(image)
         image = ut.pad_image_if_not_square(image)
         image = ut.resize_to(image, target_size)
-        image.save('target/' +  '{}.jpg'.format(ind), 'JPEG')
-        image = ut.resize_to(image, source_size)
-        image.save('input/' +  '{}.jpg'.format(ind), 'JPEG')
+        image.save('train_hr/train_hr/' +  '{}.jpg'.format(ind), 'JPEG')
+        image = ut.resize_to(image, input_size)
+        image.save('train_lr/train_lr/' +  '{}.jpg'.format(ind), 'JPEG')
